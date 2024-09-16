8acd6b0f74e97b5fb125d72f3c01427fcadcae65
emess2g <essuonemma84gmail.com>
2024-09-16 13:05:27 +0000

    removed strick mode , imported browserrouter

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from './components/About'
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="min-h-screen p- bg-[#0B0A09]">
     <div className='max-w-2xl mx-auto max-w-2xl mx-auto p-4 ' id='man' >
-    <BrowserRouter>
+    
         {/* <ScrollToTop /> */}
         <div className=" ">
           <div className="   ">
@@ -29,7 +29,7 @@ function App() {
           </div>
           <Footer />
         </div>
-      </BrowserRouter> 
+       
     </div>  
     </div>
   )
